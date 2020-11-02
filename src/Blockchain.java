935236e2890581a22f849ad2706d657554c9b63c
PauliusZ15 <paulius.z15@gmail.com>
2020-11-02 20:42:38 +0200

    add hash generator

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public class Blockchain {
 
+    private int MIN_USER_BALANCE;
+    private int MAX_USER_BALANCE;
+
     private ArrayList<User> users;
     private final int usersAmount;
 
@@ -11,12 +14,19 @@ public class Blockchain {
     }
 
     private void generateUsers() {
+        HashGenerator hashGenerator = new HashGenerator();
+
         for (int i = 0; i < usersAmount; i++) {
             String name = "User" + 1;
-            String publicKey = "";
-            double balance = 0;
+            String publicKey = hashGenerator.getHash(Integer.toString(i));
+            double balance = (Math.random() * ((MAX_USER_BALANCE - MIN_USER_BALANCE) + 1)) + MIN_USER_BALANCE;
             User newUser = new User(name, publicKey, balance);
-            this.users.add(newUser);
+
+            users.add(newUser);
         }
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
     }
 }
---
+++ src/HashGenerator.java
@@ -0,0 +1,37 @@
+public class HashGenerator {
+
+    public final int HASH_LENGTH = 64;
+
+    public String getHash(String input) {
+        int hashInt = 0;
+
+        for (int inputElem : input.toCharArray()) {
+            hashInt = stirChar(hashInt, inputElem);
+        }
+
+        StringBuilder hashBuilder = new StringBuilder(Integer.toHexString(hashInt));
+
+        while (hashBuilder.length() != HASH_LENGTH) {
+            for (int hashElem : hashBuilder.toString().toCharArray()){
+                hashInt = stirChar(hashInt, hashElem);
+
+                hashBuilder.append(Integer.toHexString(hashInt));
+            }
+
+            int hashLength = hashBuilder.length();
+            if (hashLength > HASH_LENGTH) {
+                hashBuilder.delete(HASH_LENGTH, hashLength);
+            }
+        }
+
+        return  hashBuilder.toString();
+    }
+
+    private int stirChar(int hashInt, int elem) {
+        hashInt += Math.pow(elem, 3);
+        hashInt += ~elem % ~hashInt;
+        hashInt += hashInt << ~hashInt << 1;
+
+        return hashInt;
+    }
+}
